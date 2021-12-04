5a7477adc4ec3a2aa067e1f2c588b6d8164f36f5
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2021-12-04 00:18:53 +0000

    Updated fish configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
-fset -U EDITOR emacsclient -nw --alternate-editor=""
+set -gx EDITOR emacsclient -nw --alternate-editor=.
+set -x OTM ~/work/otm
+set -x EXPERT $OTM/expert-website
 abbr -- - 'cd -'
 abbr g 'git'
 abbr gfo 'git fetch origin'
+abbr gca 'git commit --amend'
 abbr gb 'git branch'
 abbr gbr 'git branch -r'
 abbr e '$EDITOR'
-
